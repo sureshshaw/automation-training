1097ff09acda38ea941a829527a74c238fa91605
sureshshaw <sureshcsegcect@gmail.com>
2026-01-10 18:00:33 +0530

    test

--- app/robots.js
+++
@@ -1,9 +0,0 @@
-export default function robots() {
-  return {
-    rules: {
-      userAgent: "*",
-      allow: "/",
-    },
-    sitemap: "https://your-site-url.netlify.app/sitemap.xml",
-  };
-}
